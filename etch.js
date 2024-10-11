3147899ee756111118c43f71aac307aafd4ddcbb
Sandra Wang <sandrawang56@gmail.com>
2024-10-11 15:19:11 -0700

    finished etch basic

--- etch.js
+++ etch.js
@@ -1,15 +1,61 @@
+let colorChoice = 'lightGreen';
 function createGrid(n){
     const gridDiv = document.querySelector("#grid");
+    let isMouseDown = false;
+
+    gridDiv.addEventListener('mousedown', ()=>{
+        isMouseDown = true;
+
+    });
+
+    gridDiv.addEventListener('mouseup', ()=> {
+        isMouseDown = false;
+    });
+
     for(let i = 0; i<n; i++){
         for(let j = 0; j<n; j++){
             const gridSquare = document.createElement("div");
-            gridSquare.style.width = "50px";
-            gridSquare.style.height = "50px";
-            gridSquare.style.border = "1px solid black"; 
-            gridSquare.style.display = "grid";
+            gridSquare.className = "grid-square";
             gridDiv.appendChild(gridSquare);
+            gridSquare.addEventListener('mouseover', function () {
+                if(isMouseDown){
+                    gridSquare.style.backgroundColor = colorChoice;
+                }
+                
+            });
+
+
         }
     }
 }
 
-createGrid(16);
+
+function createButtons(){
+    const buttonDiv = document.querySelector("#buttons");
+    let presets = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+    for(let i= 0; i<presets.length; i++){
+        const colorButton = document.createElement("button");
+        colorButton.style["background-color"]= presets[i];
+        colorButton.className = "color-button";
+        colorButton.addEventListener('click', ()=>{
+            colorChoice = presets[i];
+        })
+        buttonDiv.appendChild(colorButton);
+    }
+}
+
+function clear(){
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach(square => {
+        square.style.backgroundColor = ""; // Clear the background color
+    });
+}
+
+function createDeleteButton(){
+    const deleteButton = document.querySelector("#delete");
+    deleteButton.addEventListener("click", ()=>{clear();});
+}
+
+createGrid(32);
+createButtons();
+createDeleteButton();
